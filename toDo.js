1ccbed4dc3fd5adf4c5328240b873665b943bc68
Marcy Villegas <58241136+marcyvillegas@users.noreply.github.com>
2021-08-06 19:40:52 +0800

    JS worked but it disappears immediately
    The js code worked but as soon as you click the button the task disappears. It does not have a delete button yet.

--- toDo.js
+++ toDo.js
@@ -1,11 +1,19 @@
 let button = document.querySelector('#submitButton'); //Declare the button element  
 
-button.addEventListener("click", function(){ //Create function when button is clicked
-    
+button.addEventListener("click", function () { //Create function when button is clicked
+
     let taskValue = document.querySelector('#taskName').value; //Declare the variable that will get the value of the taskName  
-    
-    document.querySelector("#taskContainer").innerHTML = '<li>' + taskValue + '</li> <button type="submit" id="delButton">X</button>';
+
+    let taskList = document.createElement("li");
+
+    let taskContainer = document.querySelector("#taskContainer");
+
+
+    let taskTxt = document.createTextNode(taskValue);
+    taskContainer = taskList.appendChild(taskTxt);
+    document.body.appendChild(taskContainer);
 });
+
 
 
 
