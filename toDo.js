740659ca4c755c27463fcf60ceb3d130eef0efed
Marcy Villegas <58241136+marcyvillegas@users.noreply.github.com>
2021-08-06 21:58:27 +0800

    Renamed variables and changed the function
    Renamed the variables to getElement and changed the whole function. It is still not working.

--- toDo.js
+++ toDo.js
@@ -1,3 +1,26 @@
+let button = document.getElementById("#submitButton");
+let taskValue = document.getElementById("#taskName");
+let taskList = document.getElementById("taskContainer");
+
+
+button.addEventListener("click", function(){
+    let newTask = document.createElement('p');
+    newTask.innerText = taskValue.value;
+    taskList.appendChild(newTask);
+
+    
+});
+
+
+
+/*
+let button = document.querySelector("#submitButton");
+let taskValue = document.querySelector("#taskName");
+let taskList = document.querySelector("#taskContainer");
+*/
+
+
+/*
 let button = document.querySelector('#submitButton'); //Declare the button element  
 
 button.addEventListener("click", function () { //Create function when button is clicked
@@ -8,12 +31,9 @@ button.addEventListener("click", function () { //Create function when button is 
 
     let taskContainer = document.querySelector("#taskContainer");
 
-
     let taskTxt = document.createTextNode(taskValue);
+
     taskContainer = taskList.appendChild(taskTxt);
     document.body.appendChild(taskContainer);
 });
-
-
-
-
+*/
