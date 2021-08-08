7dce3b87f4d9e275975ebe8a333e06d8d63f326c
Marcy Villegas <58241136+marcyvillegas@users.noreply.github.com>
2021-08-08 15:47:32 +0800

    The task was printed
    The code worked. querySelector is used rather than the getElement method because appendChild cannot call it.  There is no delete button yet.

--- toDo.js
+++ toDo.js
@@ -1,39 +1,13 @@
-let button = document.getElementById("#submitButton");
-let taskValue = document.getElementById("#taskName");
-let taskList = document.getElementById("taskContainer");
+let button = document.querySelector("#submitButton");
+let taskValue = document.querySelector("#taskName");
+let taskList = document.querySelector(".taskContainer");
 
 
-button.addEventListener("click", function(){
+button.addEventListener("click", function(e){
+    e.preventDefault();
     let newTask = document.createElement('p');
     newTask.innerText = taskValue.value;
     taskList.appendChild(newTask);
 
-    
 });
 
-
-
-/*
-let button = document.querySelector("#submitButton");
-let taskValue = document.querySelector("#taskName");
-let taskList = document.querySelector("#taskContainer");
-*/
-
-
-/*
-let button = document.querySelector('#submitButton'); //Declare the button element  
-
-button.addEventListener("click", function () { //Create function when button is clicked
-
-    let taskValue = document.querySelector('#taskName').value; //Declare the variable that will get the value of the taskName  
-
-    let taskList = document.createElement("li");
-
-    let taskContainer = document.querySelector("#taskContainer");
-
-    let taskTxt = document.createTextNode(taskValue);
-
-    taskContainer = taskList.appendChild(taskTxt);
-    document.body.appendChild(taskContainer);
-});
-*/
