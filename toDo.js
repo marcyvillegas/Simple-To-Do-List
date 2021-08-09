41fa14f897f7776d64ab7e9d0f4d888c44e80a52
Marcy Villegas <58241136+marcyvillegas@users.noreply.github.com>
2021-08-09 21:10:45 +0800

    Changed p to li and added delete button
    The p is changed to li and there's a delete button for every li but it is still not functioning.

--- toDo.js
+++ toDo.js
@@ -1,13 +1,23 @@
+//Declare the variables for the button, input field value and div container of the new tasks
 let button = document.querySelector("#submitButton");
 let taskValue = document.querySelector("#taskName");
 let taskList = document.querySelector(".taskContainer");
+let container = document.querySelector(".taskBox");
 
-
+//Function when button is clicked
 button.addEventListener("click", function(e){
+    //Cancel the default action
     e.preventDefault();
-    let newTask = document.createElement('p');
+    let newTask = document.createElement('li');
+    let deleteButton = document.createElement('button');
     newTask.innerText = taskValue.value;
+    deleteButton.innerText = "X";
+    deleteButton.classList.add();
+    newTask.appendChild(deleteButton);
+    container.appendChild(taskList);
     taskList.appendChild(newTask);
+    taskValue.value = "";
+
 
 });
 
