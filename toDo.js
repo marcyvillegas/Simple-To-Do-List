4242549f56c0febf89b10ca8c6b988fe83a0f025
Marcy Villegas <58241136+marcyvillegas@users.noreply.github.com>
2021-08-10 23:38:34 +0800

    Added a check button
    Added a background color and a check button but still has no function. The removed element are still present in the website. It is damaging the website's appearance.

--- toDo.js
+++ toDo.js
@@ -1,26 +1,49 @@
-//Declare the variables for the button, input field value and div container of the new tasks
-let button = document.querySelector("#submitButton");
-let taskValue = document.querySelector("#taskName");
-let taskList = document.querySelector(".taskContainer");
-let container = document.querySelector(".taskBox");
+//Declare the variables for the button, input field value and div container of ul 
+let button = document.querySelector("#submit-button");
+let taskValue = document.querySelector("#task-name");
+let taskList = document.querySelector(".task-container");
+let container = document.querySelector(".task-box");
 
 //Function when button is clicked
-button.addEventListener("click", function(e){
+button.addEventListener("click", function (e) {
+
     //Cancel the default action
     e.preventDefault();
-    let newTask = document.createElement('li');
-    newTask.setAttribute("id","newTask")
-    let deleteButton = document.createElement('button');
-    deleteButton.classList.add("delButton");
+
+    //Create new div container for li
+    let newTaskContainer = document.createElement("div");
+    newTaskContainer.classList.add("new-task-container");
+
+    //Create li element
+    let newTask = document.createElement("li");
+    newTask.classList.add("new-task");
     newTask.innerText = taskValue.value;
+
+    //Create check button 
+    let checkButton = document.createElement("button");
+    checkButton.classList.add("check-button");
+    checkButton.innerText = "OK";
+
+    //Create delete button 
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("del-button");
     deleteButton.innerText = "X";
-    newTask.appendChild(deleteButton);
+
+    //Insert elements to ul and div container for ul
+    newTaskContainer.appendChild(newTask);
+    newTaskContainer.appendChild(checkButton);
+    newTaskContainer.appendChild(deleteButton);
+    taskList.appendChild(newTaskContainer);
     container.appendChild(taskList);
-    taskList.appendChild(newTask);
+
+    //Empty input value
     taskValue.value = "";
 
-    deleteButton.addEventListener("click", function(){
-        taskList.removeChild(newTask);
+    //Function when delete button is clicked
+    deleteButton.addEventListener("click", function () {
+        newTaskContainer.removeChild(newTask);
+        newTaskContainer.removeChild(checkButton);
+        newTaskContainer.removeChild(deleteButton);
     });
 });
 
