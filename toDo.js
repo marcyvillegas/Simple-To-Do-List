694e434dcb73c53ed54a9627c89982dd9d1cc29f
Marcy Villegas <58241136+marcyvillegas@users.noreply.github.com>
2021-08-13 20:37:41 +0800

    Added animation and snack bar
    Added animation and snack bar every time the check and trash icon is clicked.

--- toDo.js
+++ toDo.js
@@ -1,9 +1,10 @@
-//Declare the variables for the button, input field value and div container of ul 
-let button = document.querySelector("#submit-button");
-let taskValue = document.querySelector("#task-name");
-let taskList = document.querySelector(".task-container");
-let wholeContainer = document.querySelector(".task-box");
-let inputContainer = document.querySelector(".todo-input");
+//Declare the variables
+let inputContainer = document.querySelector(".todo-input"); //Container of the input field
+let button = document.querySelector("#submit-button"); //Add button
+let taskValue = document.querySelector("#task-name"); //Input field
+let wholeContainer = document.querySelector(".task-box"); //Container of ul
+let taskList = document.querySelector(".task-container"); //Unordered list of the tasks
+let snackBar = document.querySelector(".snack-bar"); //Snack Bar for delete and check button
 
 //Function when button is clicked
 button.addEventListener("click", function (e) {
@@ -15,17 +16,23 @@ button.addEventListener("click", function (e) {
     let newTaskContainer = document.createElement("div");
     newTaskContainer.classList.add("new-task-container");
 
+    //Create animation for adding task
+    newTaskContainer.style.animation = "fadeIn 0.5s ease";
+    setTimeout(function () {
+        newTaskContainer.style.animation = "none";
+    }, 500);
+
     //Create li element
     let newTask = document.createElement("li");
     newTask.classList.add("new-task");
     newTask.innerText = taskValue.value;
 
-
+    //Validate if input field is empty
     if (taskValue.value.trim() == '') {
         inputContainer.setAttribute("id", "error")
         setTimeout(function () {
             inputContainer.removeAttribute("id");
-        }, 3000);
+        }, 2500);
     } else {
 
         //Create check button 
@@ -48,12 +55,41 @@ button.addEventListener("click", function (e) {
         //Empty input value
         taskValue.value = "";
 
+        //Function when check button is clicked
+        checkButton.addEventListener("click", function () {
+            newTaskContainer.style.animation = "fadeOut 0.3s ease";
+            setTimeout(function () {
+                newTaskContainer.removeChild(newTask);
+                newTaskContainer.removeChild(checkButton);
+                newTaskContainer.removeChild(deleteButton);
+                taskList.removeChild(newTaskContainer);
+                newTaskContainer.style.animation = "none";
+            }, 200);
+            
+            snackBar.innerHTML = "<p>You have completed your task.</p>";
+            snackBar.style.display = "block";
+            setTimeout(function (){
+                snackBar.style.display = "none";
+            }, 3000);
+        });
+
         //Function when delete button is clicked
         deleteButton.addEventListener("click", function () {
-            newTaskContainer.removeChild(newTask);
-            newTaskContainer.removeChild(checkButton);
-            newTaskContainer.removeChild(deleteButton);
-            taskList.removeChild(newTaskContainer);
+            newTaskContainer.style.animation = "fadeOut 0.3s ease";
+            setTimeout(function () {
+                newTaskContainer.removeChild(newTask);
+                newTaskContainer.removeChild(checkButton);
+                newTaskContainer.removeChild(deleteButton);
+                taskList.removeChild(newTaskContainer);
+                newTaskContainer.style.animation = "none";
+            }, 200);
+            
+            snackBar.innerHTML = "<p>Your task has been deleted.</p>";
+            snackBar.style.display = "block";
+            setTimeout(function (){
+                snackBar.style.display = "none";
+            }, 3000);
+
         });
     }
 });
